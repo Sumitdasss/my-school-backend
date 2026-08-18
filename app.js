203612211070120abffa360951968b6440edadd4
Sumitdasss <sumitdas46123@gmail.com>
2026-08-18 21:25:53 +0600

    add

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(
   cors({
     origin: [
       "https://my-school-hc7r.vercel.app",
-      "http://localhost:3000",
+    
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
