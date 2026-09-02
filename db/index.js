fdb0bf0c0245cf43a2774b15b09108215ca7288c
Sumitdasss <sumitdas46123@gmail.com>
2026-09-02 11:34:07 +0600

    add

--- db/index.js
+++ db/index.js
@@ -1,8 +1,37 @@
 import "dotenv/config";
 
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import ws from "ws";
+import {
+  Pool,
+  neonConfig,
+} from "@neondatabase/serverless";
 
-const sql = neon(process.env.DATABASE_URL);
+import { drizzle } from "drizzle-orm/neon-serverless";
 
-export const db = drizzle(sql);
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing");
+}
+
+
+// Node.js environment-এর জন্য WebSocket
+neonConfig.webSocketConstructor = ws;
+
+
+// Create Pool
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+
+  // Vercel Serverless-এর জন্য ছোট pool
+  max: 1,
+
+  idleTimeoutMillis: 10000,
+
+  connectionTimeoutMillis: 10000,
+});
+
+
+// Connect Drizzle with Pool
+export const db = drizzle({
+  client: pool,
+});
